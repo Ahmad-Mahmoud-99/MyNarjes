84d0efd8345a939d0ca89766c510e1bb8f7c31b1
AhmadShon <ahmadshon87@gmail.com>
2021-02-14 10:38:03 +0200

    alert error

--- resources/views/alert/errors.blade.php
+++ resources/views/alert/errors.blade.php
@@ -1,6 +1,6 @@
 @if(Session::has('error'))
-<div class="row mr-2 ml-2">
- <button type="text" class="btn btn-lg btn-block btn-outline-danger mb-2"id="type-error">  
+<div class="row mr-2 ml-2" cursor = "default">
+ <button type="text" class="btn btn-lg btn-block btn-outline-danger mb-2"id="type-error"  disabled>
       {{Session::get('error')}}
  </button>
  </div>
