a7d852e88ca96ba2425dc82f3181448f5343aad8
jannatOmar <jannat.omar2017@gmail.com>
2021-02-14 03:04:52 -0800

    jjj

--- routes/admin.php
+++ routes/admin.php
@@ -13,28 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-});
-
-Auth::routes();
-// Route::group(['namespace'=>'Admin','middleware'=>'auth:admin'],function(){
-//     Route::get('/admin','DashboardController@index')->name('admin.dashboard');
-//     ####################patient managment##############
-//     Route::group(['prefix'=>'patientManagment'],function(){
-//         Route::get('/','patientManagmentController@index')->name('admin.patientManagment');
-//         Route::get('create','patientManagmentController@create')->name('admin.patientManagment.create');
-//         Route::post('store','patientManagmentController@store')->name('admin.patientManagment.store');
- 
-        
-        // Route::get('edit/{id}','patientManagmentController@edit')->name('admin.patientManagment.edit');
-        // Route::post('update/{id}','patientManagmentController@update')->name('admin.patientManagment.update');//or put
-        // Route::get('delete/{id}','patientManagmentController@destroy')->name('admin.patientManagment.delete');
- 
- 
-    //  });
-=======
 // Route::get('/admin', function () {
 //     return view('admin.dashboard');
 // });
@@ -56,7 +34,6 @@ Route::group([/*, 'middleware'=>'auth:admin'*/],function(){
 
 
      });
->>>>>>> 3980009638152815eb2bef23406203e1109afb7e
 
 // });
 
