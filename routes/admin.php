f005814a983274b25af10b6e783decda5d9ab9a1
jannatOmar <jannat.omar2017@gmail.com>
2021-02-13 21:29:56 -0800

    login-patientmanagment

--- routes/admin.php
+++ routes/admin.php
@@ -13,18 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/admin', function () {
-//     return view('admin.dashboard');
-// });
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
 
 Auth::routes();
-Route::group(['namespace'=>'Admin','middleware'=>'auth:admin'],function(){
-    Route::get('/admin','DashboardController@index')->name('admin.dashboard');
-    ####################patient managment##############
-    Route::group(['prefix'=>'patientManagment'],function(){
-        Route::get('/','patientManagmentController@index')->name('admin.patientManagment');
-        Route::get('create','patientManagmentController@create')->name('admin.patientManagment.create');
-        Route::post('store','patientManagmentController@store')->name('admin.patientManagment.store');
+// Route::group(['namespace'=>'Admin','middleware'=>'auth:admin'],function(){
+//     Route::get('/admin','DashboardController@index')->name('admin.dashboard');
+//     ####################patient managment##############
+//     Route::group(['prefix'=>'patientManagment'],function(){
+//         Route::get('/','patientManagmentController@index')->name('admin.patientManagment');
+//         Route::get('create','patientManagmentController@create')->name('admin.patientManagment.create');
+//         Route::post('store','patientManagmentController@store')->name('admin.patientManagment.store');
  
         
         // Route::get('edit/{id}','patientManagmentController@edit')->name('admin.patientManagment.edit');
@@ -32,9 +32,9 @@ Route::group(['namespace'=>'Admin','middleware'=>'auth:admin'],function(){
         // Route::get('delete/{id}','patientManagmentController@destroy')->name('admin.patientManagment.delete');
  
  
-     });
+    //  });
 
-});
+// });
 
 // Route::get('/home', 'HomeController@index')->name('home');
 
