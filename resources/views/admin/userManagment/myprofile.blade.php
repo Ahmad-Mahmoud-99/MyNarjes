f8ffb6fdfadeb2fa0aca20f1dd7c9da52e85fae2
aroubahmad <aroubahmad159357@gmail.com>
2021-02-15 23:11:38 +0200

    Myprofile

--- resources/views/admin/userManagment/myprofile.blade.php
+++ resources/views/admin/userManagment/myprofile.blade.php
@@ -3,7 +3,7 @@
 @section('dash','My Profile')
 @section('content')
 <div class="row">
-            <div class="col-md-8" style="margin: auto;">
+        <div class="col-md-8" style="margin: auto;">
               <div class="card" style="width:120%; margin-left: -40px;">
                 <div class="card-header card-header-icon card-header-rose">
                   <div class="card-icon">
@@ -12,102 +12,90 @@
                   <h4 class="card-title">My Profile
                     
                   </h4>
+                  <br>
                 </div>
                 <div class="card-body">
                   <form>
-                   
-                   <br> <div class="row">
-                      <div class="col-md-4">
+                    <div class="row">
+                      <label class="col-sm-3 col-form-label">First Name :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">First name</label>
-                          <input type="text" class="form-control" >
+                          <input type="text" class="form-control" value="Disabled" disabled>
                         </div>
                       </div>
-                      <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label">Middle Name :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Middle name</label>
-                          <input type="text" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Last name</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value="Disabled " disabled>
                         </div>
                       </div>
                     </div>
                     <br>
                     <div class="row">
-                      <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label">Last Name :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">User name</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value="Disabled " disabled>
                         </div>
                       </div>
-                      <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label">User Name :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Password</label>
-                          <input type="password" class="form-control">
-                        </div>
-                      </div>
-
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Email</label>
-                          <input type="email" class="form-control">
+                          <input type="text" class="form-control" value="Disabled" disabled>
                         </div>
                       </div>
                     </div>
                     <br>
-                   
-                    
                     <div class="row">
-                    <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label">Password :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">phone</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value="Disabled " disabled>
                         </div>
                       </div>
-                      <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label"> User Email :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Address</label>
-                          <input type="text" class="form-control">
-                        </div>
-                      </div>
-                      
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Role</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value="Disabled " disabled>
                         </div>
                       </div>
                     </div>
                     <br>
-
                     <div class="row">
-                    <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label">Address :</label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">age</label>
-                          <input type="text" class="form-control">
+                          <input type="text" class="form-control" value="Disabled" disabled>
                         </div>
                       </div>
-                      <div class="col-md-4">
+                      <label class="col-sm-3 col-form-label"> User Role : </label>
+                      <div class="col-sm-3">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Start date</label><br>
-                          <input type="date" class="form-control">
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">End date</label><br>
-                          <input type="date" class="form-control">
+                          <input type="text" class="form-control" value="Disabled " disabled>
                         </div>
                       </div>
                       
                     </div>
                     <br>
-                    
+                    <div class="row">
+                      <label class="col-sm-3 col-form-label">Start Date :</label>
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                          <input type="text" class="form-control" value="Disabled" disabled>
+                        </div>
+                      </div>
+                      <label class="col-sm-3 col-form-label">End Date :</label>
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                          <input type="text" class="form-control" value="Disabled " disabled>
+                        </div>
+                      </div>
+                      
+                    </div>
+                    <br><br>
+
                   </form>
                 </div>
               </div>
+ 
 @endsection
